a5632985dc367df9f98c09c97c4d82e8aa3296b6
hyccpq <hyccpq@hotmail.com>
2019-07-29 22:33:33 +0800

    2.5.0打包1,去除lock

--- server/index.js
+++ server/index.js
@@ -65,15 +65,15 @@ console.log(MIDDLEWARES);
 
     useMiddlewares(app)
 
-    // if (process.env.NODE_ENV !== 'production') {
+    if (process.env.NODE_ENV !== 'production') {
         app.listen(8088, () => {
             console.log('服务运行于\nhttp://localhost:8088');
         })
-    // } else {
-    //     http2.createSecureServer(HTTPS_OPTIONS, app.callback()).listen(443, () => {
-    //         console.log("https://localhost:443".bgRed);
-    //     });
-    // }
+    } else {
+        http2.createSecureServer(HTTPS_OPTIONS, app.callback()).listen(443, () => {
+            console.log("https://localhost:443".bgRed);
+        });
+    }
 
 
 })()
