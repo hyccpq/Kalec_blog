f980e9495c4a96bdf2e1cb5a0336fc8a10e15cae
hyccpq <hyccpq@hotmail.com>
2020-07-03 17:55:37 +0800

    tsl add

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import { resolve } from 'path'
 import R from 'ramda'
 import http2 from 'http2'
 import fs from 'fs'
+import GreenLock from 'greenlock-koa'
+
 import {
   initSchemas,
   initAdmin,
@@ -11,18 +13,19 @@ import {
 } from './database/init.js'
 import 'colors'
 import { getDirname, getRequire } from './lib/file.js'
+import { userInfo } from './conf/userConf'
 
 const MIDDLEWARES = ['utils', 'session', 'staticServer', 'router']
 
-const HTTPS_OPTIONS = {
-  key: fs.readFileSync(
-    resolve(getDirname(import.meta).__dirname, './conf/ssl/private.key')
-  ),
-  cert: fs.readFileSync(
-    resolve(getDirname(import.meta).__dirname, './conf/ssl/full_chain.crt')
-  ),
-  allowHTTP1: true
-}
+// const HTTPS_OPTIONS = {
+//   key: fs.readFileSync(
+//     resolve(getDirname(import.meta).__dirname, '/conf/ssl./conf/ssl/private.key')
+//   ),
+//   cert: fs.readFileSync(
+//     resolve(getDirname(import.meta).__dirname, '/conf/ssl./conf/ssl/full_chain.crt')
+//   ),
+//   allowHTTP1: true
+// }
 
 const condition = process.env.NODE_ENV
 
@@ -44,6 +47,7 @@ switch (condition) {
 
 const app = new Koa()
 console.log(MIDDLEWARES)
+// console.log(GreenLock)
 ;(async () => {
   await initSchemas()
 
@@ -76,7 +80,10 @@ console.log(MIDDLEWARES)
         R.compose(
           s => import(s),
           name =>
-            resolve(getDirname(import.meta).__dirname, `./middlewares/${name}`),
+            resolve(
+              getDirname(import.meta).__dirname,
+              `/conf/ssl./middlewares/${name}`
+            ),
           iter => `${iter}.js`
         )
       ),
@@ -92,11 +99,61 @@ console.log(MIDDLEWARES)
       console.log('服务运行于\nhttp://kalec.kalecgos.top:8088')
     })
   } else {
+    const greenlock = GreenLock.create({
+      version: 'draft-11', // Let's Encrypt v2
+      // You MUST change this to 'https://acme-v02.api.letsencrypt.org/directory' in production
+      server: 'https://acme-staging-v02.api.letsencrypt.org/directory',
+
+      email: userInfo.email,
+      agreeTos: true,
+      approveDomains: ['kalecgos.top'],
+      // store: await import('greenlock-store-fs'),
+      // Join the community to get notified of important updates
+      // and help make greenlock better
+      communityMember: true,
+
+      configDir: resolve(getDirname(import.meta).__dirname, `./conf/ssl`),
+      domainKeyPath: resolve(
+        getDirname(import.meta).__dirname,
+        './conf/ssl/privkey.pem'
+      ),
+      privkeyPath: resolve(
+        getDirname(import.meta).__dirname,
+        './conf/ssl/privkey.pem'
+      ),
+      fullchainPath: resolve(
+        getDirname(import.meta).__dirname,
+        './conf/ssl/fullchain.pem'
+      ),
+      certPath: resolve(
+        getDirname(import.meta).__dirname,
+        './conf/ssl/cert.pem'
+      ),
+      chainPath: resolve(
+        getDirname(import.meta).__dirname,
+        './conf/ssl/chain.pem'
+      ),
+      bundlePath: resolve(
+        getDirname(import.meta).__dirname,
+        './conf/ssl/bundle.pem'
+      )
+
+      //, debug: true
+
+      //, debug: true
+    })
     app.listen(80, () => {
       console.log('服务运行于\nhttp://localhost:80')
     })
-    http2.createSecureServer(HTTPS_OPTIONS, app.callback()).listen(443, () => {
-      console.log('https://localhost:443'.bgRed)
-    })
+    console.log(';;;;;;;;;')
+    console.log(greenlock)
+    http2
+      .createSecureServer(
+        greenlock.tlsOptions,
+        greenlock.middleware(app.callback())
+      )
+      .listen(443, () => {
+        console.log('https://localhost:443'.bgRed)
+      })
   }
 })()
